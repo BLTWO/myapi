8b24a4d451dac1653ea9bcef0c6c20b99bb9fede
BLTWO <74575629+BLTWO@users.noreply.github.com>
2025-08-19 00:40:32 -0400

    created all apps and service acc

---
+++ myapi/resume/views.py
@@ -0,0 +1,18 @@
+from rest_framework import viewsets, status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from .models import Resume
+from .serializers import ResumeSerializer
+
+
+class ResumeViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = ResumeSerializer
+
+    def list(self, request):
+        resume = Resume.objects.first()
+        if not resume:
+            return Response(
+                {"detail": "Resume not found."}, status=status.HTTP_404_NOT_FOUND
+            )
+        return Response(ResumeSerializer(resume).data, status=status.HTTP_200_OK)
