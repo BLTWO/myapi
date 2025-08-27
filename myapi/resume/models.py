818e1223497bb794f42a4d54a0dc87a33a16b91d
BLTWO <74575629+BLTWO@users.noreply.github.com>
2025-08-27 00:17:45 -0400

    rename myapi/myapi to myapi/src

--- myapi/resume/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Resume(models.Model):
-    resume_uri = models.URLField(default='', blank=True)
-    date = models.DateTimeField(null=True, blank=True)
