27eec925b03ee47b739f7ef3a14f38f05e88d35b
BLTWO <74575629+BLTWO@users.noreply.github.com>
2025-08-29 17:52:30 -0400

    3

--- src/myapi/urls.py
+++ src/myapi/urls.py
@@ -46,7 +46,7 @@ def api_root(request, format=None):
     })
 
 urlpatterns = [
-    path("", lambda request: redirect("api/", permanent=False)),
+    #path("", lambda request: redirect("api/", permanent=False)),
     path("admin/", admin.site.urls),
     path("api/", api_root),
     path("api/", include(router.urls)),
